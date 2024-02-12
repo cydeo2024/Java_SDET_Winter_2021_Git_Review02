6e129c232151691c34f6f7df7b20a2dc038ec262
Madina <mdavlatshoeva84@gmail.com>
2024-02-11 23:13:42 -0500

    made changes to Hello World

--- src/git_review01/HelloWorld.java
+++ src/git_review01/HelloWorld.java
@@ -3,5 +3,6 @@ package git_review01;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("This is Hello World class");
+        System.out.println("Hello World");
     }
 }
